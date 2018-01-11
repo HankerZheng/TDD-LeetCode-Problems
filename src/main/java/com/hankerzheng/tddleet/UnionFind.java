28075d4af665c47eceeeeea29aaca1e0ba49d072
Hanker Zheng <zhenghan991@gmail.com>
2018-01-11 00:06:45 -0800

    UnionFind java implementation with union size count

--- src/main/java/com/hankerzheng/tddleet/UnionFind.java
+++ src/main/java/com/hankerzheng/tddleet/UnionFind.java
@@ -1,4 +1,48 @@
 package com.hankerzheng.tddleet;
 
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * UnionFind implementation with number count.
+ *
+ */
 public class UnionFind {
+
+    private int[] status;
+
+    public UnionFind(final int n) {
+        status = new int[n + 1];
+        for (int i = 0; i < status.length; i++) {
+            status[i] = -1;
+        }
+    }
+
+
+    public void union(final int a, final int b) {
+        final int parentA = find(a);
+        final int parentB = find(b);
+        status[parentA] += status[parentB];
+        status[parentB] = parentA;
+    }
+
+    public int find(final int a) {
+        if (a < 0) {
+            return a;
+        }
+        final List<Integer> setA = new LinkedList<>();
+        int thisNode = a;
+        while (status[thisNode] > 0) {
+            setA.add(thisNode);
+            thisNode = status[thisNode];
+        }
+        for(int node: setA) {
+            status[node] = thisNode;
+        }
+        return thisNode;
+    }
+
+    public int count(final int a) {
+        return -status[find(a)];
+    }
 }
--- src/test/java/com/hankerzheng/tddleet/UnionFindTest.java
+++ src/test/java/com/hankerzheng/tddleet/UnionFindTest.java
@@ -1,5 +1,41 @@
+package com.hankerzheng.tddleet;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class UnionFindTest {
 
+    @Test
+    public void testUnionFindWithSizeOne() throws Exception {
+        final UnionFind unionFind = new UnionFind(1);
+        assertEquals(1, unionFind.find(1) );
+        assertEquals(1, unionFind.count(1));
+    }
+
+    @Test
+    public void testUnionFindWithSizeTwo() throws Exception {
+        final UnionFind unionFind = new UnionFind(2);
+        assertEquals(1, unionFind.find(1));
+        assertEquals(2, unionFind.find(2));
+        unionFind.union(1, 2);
+        assertTrue(unionFind.find(1) == unionFind.find(2));
+        assertEquals(2, unionFind.count(1));
+    }
+
+    @Test
+    public void testUnionFindWithSizeThree() throws Exception {
+        final UnionFind unionFind = new UnionFind(3);
+        assertEquals(1, unionFind.find(1));
+        assertEquals(2, unionFind.find(2));
+        assertEquals(3, unionFind.find(3));
+        unionFind.union(1, 2);
+        unionFind.union(1, 3);
+        assertTrue(unionFind.find(1) == unionFind.find(2));
+        assertTrue(unionFind.find(3) == unionFind.find(2));
+        assertEquals(3, unionFind.count(1));
+        assertEquals(3, unionFind.count(2));
+        assertEquals(3, unionFind.count(3));
+
+    }
 }
