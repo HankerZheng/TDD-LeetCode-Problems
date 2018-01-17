87d0eb67e1d2fa36ff6b51ee5cb7ea9f5b575898
Hanker Zheng <zhenghan991@gmail.com>
2018-01-17 00:29:01 -0800

    Common class for TreeNode

---
+++ src/main/java/com/hankerzheng/tddleet/TreeNode.java
@@ -0,0 +1,53 @@
+package com.hankerzheng.tddleet;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    static public TreeNode constructOneNodeBST(int val) {
+        return new TreeNode(val);
+    }
+
+    static public TreeNode consturctLeftTwoNodeBST() {
+        final TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        return root;
+    }
+
+    static public TreeNode consturctRightTwoNodeBST() {
+        final TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        return root;
+    }
+
+    static public TreeNode constructEasyBST() {
+        final TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        return root;
+    }
+
+    static public TreeNode constructBST1() {
+        final TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.right.left = new TreeNode(5);
+        root.right.right = new TreeNode(7);
+        return root;
+    }
+
+    static public TreeNode constructBT1() {
+        final TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.right = new TreeNode(3);
+        return root;
+    }
+}
